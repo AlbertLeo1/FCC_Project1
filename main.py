90b715f260c9f76d6a7af258f777f71c6753cbf5
AlbertLeo1 <albertohyna@gmail.com>
2024-09-07 05:32:38 +0300

    complete code. main.py is test while mean_var_std.py is the main code

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+'''
+Create a function named calculate() in mean_var_std.py that uses Numpy to output the mean, variance, 
+standard deviation, max, min, and sum of the rows, columns, and elements in a 3 x 3 matrix.
+The input of the function should be a list containing 9 digits. 
+The function should convert the list into a 3 x 3 Numpy array, 
+
+and then return a dictionary containing the mean, variance, standard deviation, max, min, 
+and sum along both axes and for the flattened matrix.
+
+The returned dictionary should follow this format:
+
+{
+  'mean': [axis1, axis2, flattened],
+  'variance': [axis1, axis2, flattened],
+  'standard deviation': [axis1, axis2, flattened],
+  'max': [axis1, axis2, flattened],
+  'min': [axis1, axis2, flattened],
+  'sum': [axis1, axis2, flattened]
+}
+'''
+
+
+
+import numpy as np
+
 def calculate(list): 
     if len(list) > 9:
         raise ValueError("Count of values entered exceeds 9")
@@ -21,3 +46,6 @@ def calculate(list):
     }
     return dict;
     
+    
+
+print(calculate([1,3,4,3,4,3,4,4,5]))
